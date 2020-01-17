10b5aa5a3fb9888bcc3c99ada61cd6a3893c73ea
nam1665 <namhoai1995@gmail.com>
2020-01-17 14:33:38 +0700

    update enhanced

--- img_crop_enhanced.py
+++ img_crop_enhanced.py
@@ -1,6 +1,52 @@
 from rotator import rotate
 from PIL import Image
 import os
+import cv2
+
+# Automatic brightness and contrast optimization with optional histogram clipping
+def automatic_brightness_and_contrast(image, clip_hist_percent=25):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray],[0],None,[256],[0,256])
+    hist_size = len(hist)
+
+    # Calculate cumulative distribution from the histogram
+    accumulator = []
+    accumulator.append(float(hist[0]))
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index -1] + float(hist[index]))
+
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum/120.0)
+    clip_hist_percent /= 2.5
+
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+
+    # Locate right cut
+    maximum_gray = hist_size -1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
+
+    '''
+    # Calculate new histogram with desired range and show histogram 
+    new_hist = cv2.calcHist([gray],[0],None,[256],[minimum_gray,maximum_gray])
+    plt.plot(hist)
+    plt.plot(new_hist)
+    plt.xlim([0,256])
+    plt.show()
+    '''
+
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return (auto_result, alpha, beta)
 
 
 def imgage_crop_enhanced(image_path_detect):
@@ -16,11 +62,19 @@ def imgage_crop_enhanced(image_path_detect):
                 os.makedirs(all_folder)
             region = img.crop((result[1][0],result[1][2], result[1][1], result[1][3] ))
             region.save(all_folder + name.split('.')[0] + "_all.jpg")
+            crop_img = cv2.imread(all_folder + name.split('.')[0] + "_all.jpg")
+            auto_result, alpha, beta = automatic_brightness_and_contrast(crop_img)
+            cv2.imwrite(all_folder + name.split('.')[0] + "_all.jpg", auto_result)
+
         if(result[2] is not None):
             if not os.path.exists(text_folder):
                 os.makedirs(text_folder)
             region = img.crop((result[2][0],result[2][2], result[2][1], result[2][3] ))
             region.save(text_folder + name.split('.')[0] + "_text.jpg")
+            crop_img = cv2.imread(text_folder + name.split('.')[0] + "_text.jpg")
+            auto_result, alpha, beta = automatic_brightness_and_contrast(crop_img)
+            cv2.imwrite(text_folder + name.split('.')[0] + "_text.jpg", auto_result)
+
     elif(result[0] == "flip_180"):
         if not os.path.exists(rotated_folder):
             os.makedirs(rotated_folder)
@@ -34,11 +88,17 @@ def imgage_crop_enhanced(image_path_detect):
                 os.makedirs(all_folder)
             region = flip_img.crop((flip_result[1][0],flip_result[1][2], flip_result[1][1], flip_result[1][3] ))
             region.save(all_folder + name.split('.')[0] + "_rotated_180_all.jpg")
+            crop_img = cv2.imread(all_folder + name.split('.')[0] + "_rotated_180_all.jpg")
+            auto_result, alpha, beta = automatic_brightness_and_contrast(crop_img)
+            cv2.imwrite(all_folder + name.split('.')[0] + "_rotated_180_all.jpg", auto_result)
         if(flip_result[2] is not None):
             if not os.path.exists(text_folder):
                 os.makedirs(text_folder)
             region = flip_img.crop((flip_result[2][0],flip_result[2][2], flip_result[2][1], flip_result[2][3] ))
             region.save(text_folder + name.split('.')[0] + "_rotated_180_text.jpg")
+            crop_img = cv2.imread(text_folder + name.split('.')[0] + "_rotated_180_text.jpg")
+            auto_result, alpha, beta = automatic_brightness_and_contrast(crop_img)
+            cv2.imwrite(text_folder + name.split('.')[0] + "_rotated_180_text.jpg", auto_result)
     elif(result[0] == "rorate_90"):
         if not os.path.exists(rotated_folder):
             os.makedirs(rotated_folder)
@@ -52,11 +112,17 @@ def imgage_crop_enhanced(image_path_detect):
                 os.makedirs(all_folder)
             region = flip_img.crop((flip_result[1][0],flip_result[1][2], flip_result[1][1], flip_result[1][3] ))
             region.save(all_folder + name.split('.')[0] + "_rotated_90_all.jpg")
+            crop_img = cv2.imread(all_folder + name.split('.')[0] + "_rotated_90_all.jpg")
+            auto_result, alpha, beta = automatic_brightness_and_contrast(crop_img)
+            cv2.imwrite(all_folder + name.split('.')[0] + "_rotated_90_all.jpg", auto_result)
         if(flip_result[2] is not None):
             if not os.path.exists(text_folder):
                 os.makedirs(text_folder)
             region = flip_img.crop((flip_result[2][0],flip_result[2][2], flip_result[2][1], flip_result[2][3] ))
             region.save(text_folder + name.split('.')[0] + "_rotated_90_text.jpg")
+            crop_img = cv2.imread(text_folder + name.split('.')[0] + "_rotated_90_text.jpg")
+            auto_result, alpha, beta = automatic_brightness_and_contrast(crop_img)
+            cv2.imwrite(text_folder + name.split('.')[0] + "_rotated_90_text.jpg", auto_result)
     elif(result[0] == "rorate_270"):
         if not os.path.exists(rotated_folder):
             os.makedirs(rotated_folder)
@@ -70,11 +136,17 @@ def imgage_crop_enhanced(image_path_detect):
                 os.makedirs(all_folder)
             region = flip_img.crop((flip_result[1][0],flip_result[1][2], flip_result[1][1], flip_result[1][3] ))
             region.save(all_folder + name.split('.')[0] + "_rotated_270_all.jpg")
+            crop_img = cv2.imread(all_folder + name.split('.')[0] + "_rotated_270_all.jpg")
+            auto_result, alpha, beta = automatic_brightness_and_contrast(crop_img)
+            cv2.imwrite(all_folder + name.split('.')[0] + "_rotated_270_all.jpg", auto_result)
         if(flip_result[2] is not None):
             if not os.path.exists(text_folder):
                 os.makedirs(text_folder)
             region = flip_img.crop((flip_result[2][0],flip_result[2][2], flip_result[2][1], flip_result[2][3] ))
             region.save(text_folder + name.split('.')[0] + "_rotated_270_text.jpg")
+            crop_img = cv2.imread(text_folder + name.split('.')[0] + "_rotated_270_text.jpg")
+            auto_result, alpha, beta = automatic_brightness_and_contrast(crop_img)
+            cv2.imwrite(text_folder + name.split('.')[0] + "_rotated_270_text.jpg", auto_result)
 
         return "ok"
 
